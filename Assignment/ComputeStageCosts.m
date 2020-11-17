c02396794975ae44628a09b2ff185b5ff3a7aad4
corkillj <corkillj@student.ethz.ch>
2020-11-17 13:56:41 +0100

    Update ComputeStageCosts.m

--- Assignment/ComputeStageCosts.m
+++ Assignment/ComputeStageCosts.m
@@ -28,6 +28,122 @@ function G = ComputeStageCosts(stateSpace, map)
     global NORTH SOUTH EAST WEST HOVER
     global K
     global TERMINAL_STATE_INDEX
+ %Amount of Pages
+L = 5;
 
+%Wind Probability
+no_wind = 1 - P_WIND;
+wind = P_WIND/4;
+
+%Index of Special Points
+id_terminal = ComputeTerminalStateIndex(stateSpace,map);
+
+[m_pick_up, n_pick_up] = find(map == PICK_UP);
+id_pick_up_nopack = getStateSpaceIndex(stateSpace , m_pick_up,n_pick_up,0);
+id_pick_up_pack = getStateSpaceIndex(stateSpace , m_pick_up,n_pick_up,1);
+
+[m_shooters,n_shooters] = find(map == SHOOTER);
+amount_shooters = length(m_shooters);
+
+[m_base, n_base]= find(map == BASE);
+id_base = getStateSpaceIndex(stateSpace , m_base,n_base,0);
+
+%Initialize P-Matrix with Zeros
+G = zeros(K,L);
+
+%Loop over all positions and determine the probability for destinations
+%depending on the input L (Hop north, east ...ect)
+for k_current = 1:K
+    
+    %Coordinates of the current field
+    m_cur = stateSpace(k_current,1);
+    n_cur = stateSpace(k_current,2);
+    package_cur = stateSpace(k_current,3);
+    
+    %Special edge case if we are at the terminal state (Drop Off field with a package on board)
+    if k_current == id_terminal
+        G(k_current,:) = 0;
+        continue;
+    end
+    
+    %For every k_current field we loop through the possible Inputs  
+    for l = 1:L
+        %Hover Move
+        m_move = 0;
+        n_move = 0;
+        if l == NORTH
+            n_move = 1;
+        elseif l == SOUTH
+            n_move = -1;
+        elseif l == EAST
+            m_move = 1;
+        elseif l == WEST
+            m_move = -1;
+        end
+        
+        %add the moves to get the position after the input
+        m_hop = m_cur + m_move;
+        n_hop = n_cur + n_move;
+        
+        %get the index of where we are after the input in order to sort out
+        %all illegal moves (out of bounds or into tree)
+        id_hop = getStateSpaceIndex(stateSpace, m_hop , n_hop,package_cur);  
+        
+        %if id_hop is empty it was an Illegal hop -> P = 0
+         if isempty(id_hop)
+             G(k_current,l) = inf;
+         else 
+            p_crash = 0;
+            
+            %Loop through all possible wind directions(including no wind)
+            %and calculate the probabiliy to reach either one of the
+            %neighboring fields or go back to base
+            for wind_dir = 1:5
+                m_dest = m_hop;
+                n_dest = n_hop;
+                p_wind = 0;
+                if wind_dir == NORTH
+                    n_dest = n_dest + 1;
+                    p_wind =  wind;
+                elseif wind_dir == SOUTH
+                    n_dest = n_dest - 1;
+                    p_wind =  wind;
+                elseif wind_dir == EAST
+                    m_dest = m_dest + 1;
+                    p_wind =  wind;
+                elseif wind_dir == WEST
+                    m_dest = m_dest - 1;
+                    p_wind =  wind;
+                elseif wind_dir == HOVER
+                    p_wind = no_wind;
+                end
+                id_dest = getStateSpaceIndex(stateSpace, m_dest , n_dest,package_cur);
+                %Case where we move into a tree or out of the map
+                if isempty(id_dest)
+                    p_crash = p_crash + p_wind ;    
+                else
+                %loop through shooters
+                    for shooter = 1:amount_shooters
+                        d = abs(m_dest - m_shooters(shooter)) + abs(n_dest - n_shooters(shooter));
+                        if (d <= R && d >= 0)
+                            probability_shot = GAMMA/(d+1);
+                            probability_shotandwind = p_wind *probability_shot;
+                     
+                            p_crash = p_crash + probability_shotandwind;
+                        end
+                    end
+                end
+           end   
+            
+           G(k_current,l)= p_crash*10 + (1-p_crash);
+            
+            
+        end
+    end
+end
+end
+
+function id = getStateSpaceIndex(stateSpace, m, n, package)
+    id = find(stateSpace(:, 1) == m & stateSpace(:, 2) == n & stateSpace(:, 3) == package);
 end
 
