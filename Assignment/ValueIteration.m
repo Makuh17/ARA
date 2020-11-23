bace50a52eda59f697d48153825a26de84ca28b2
corkillj <corkillj@student.ethz.ch>
2020-11-23 16:59:39 +0100

    Update ValueIteration.m

--- Assignment/ValueIteration.m
+++ Assignment/ValueIteration.m
@@ -28,40 +28,22 @@ function [ J_opt, u_opt_ind ] = ValueIteration(P, G)
 %       	input for each element of the state space. Mapping of the
 %       	terminal state is arbitrary (for example: HOVER).
 global K HOVER
- J = zeros(K,1);
- u = 5*ones(K,1);
- loop = true;
- while loop
-     J_old = J;
-     for i = 1:K
-         %VALUE UPDATE
-         sum_part_val = 0;
-         for j = 1:K
-             sum_part_val = sum_part_val + P(i,j,u(i))*J(j); 
-         end
-         J(i) = G(i,u(i))+ sum_part_val;
-         
-         %POLICY IMPROVEMENT
-         u_pos_val = zeros(5,1);
-         for u_count = 1:5
-             sum_part_policy =0;
-             for j = 1:K
-                 sum_part_policy = sum_part_policy + P(i,j,u_count)*J(j);
-             end
-             u_pos_val(u_count) = G(i,u_count) + sum_part_policy;
-         end
-         %u(i) = find(min(u_pos_val)==u_pos_val);
-         [~,u(i)]= min(u_pos_val);
-     end
-     %check if we are withing error threshhold
-     J_diff = abs(J - J_old);
-     loop = max(J_diff) > 10^(-5);
-     
- end
- u_opt_ind = u;
- J_opt= J;
+J_opt = zeros(K,1);
+J_diff = ones(K,1);
+u_opt_ind = 5*ones(K,1);
 
-
+while max(J_diff) > 10^(-5)
+    J_old = J_opt;
+    for i = 1:K
+        %VALUE UPDATE
+        J_opt(i) = G(i,u_opt_ind(i))+ P(i,:,u_opt_ind(i))*J_opt;
+        
+        %POLICY IMPROVEMENT
+        [~,u_opt_ind(i)]= min(G(i,:)+ reshape(pagemtimes(P(i,:,:),J_opt),[1,5]));
+    end
+    %Calculate difference for threshhold
+    J_diff = abs(J_opt - J_old);
+end
 %% Handle terminal state
 % Do yo need to do something with the teminal state before starting policy
 % iteration ?
@@ -69,5 +51,4 @@ global TERMINAL_STATE_INDEX
 % IMPORTANT: You can use the global variable TERMINAL_STATE_INDEX computed
 % in the ComputeTerminalStateIndex.m file (see main.m)
 u_opt_ind(TERMINAL_STATE_INDEX)= HOVER;
-
 end
